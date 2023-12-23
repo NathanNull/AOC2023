92689cf81998de201052aa5e201655b8edd28afb
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2023-12-23 12:05:41 -0800

    day 18 clever math thingy

--- src/day18.rs
+++ src/day18.rs
@@ -1,5 +1,3 @@
-use std::collections::{HashSet, VecDeque};
-
 pub fn pt1(input: String) {
     main(input, true);
 }
@@ -28,17 +26,13 @@ impl Direction {
         }
     }
 
-    fn offset(&self) -> (i32, i32) {
+    fn offset(&self) -> (i64, i64) {
         match self {
             Up => (0, -1),
             Down => (0, 1),
             Right => (1, 0),
             Left => (-1, 0),
         }
-    }
-
-    fn all() -> Vec<Self> {
-        vec![Up,Down,Left,Right]
     }
 }
 
@@ -48,100 +42,58 @@ impl Direction {
 // Some neat math to look at here I think
 
 fn main(input: String, pt1: bool) {
-    let instructions: Vec<(Direction, usize, &str)> = input
+    let instructions: Vec<_> = input
         .lines()
         .map(|line| {
             let [dir, num, col] = line.split(' ').collect::<Vec<_>>()[..] else {
                 panic!("Invalid input!")
             };
+            let (mut d, mut n) = (dir.to_string(), num.to_string());
+            if !pt1 {
+                let mut cnum = col.split(['(', '#', ')']).nth(2).unwrap().chars();
+                d = "RDLU"
+                    .chars()
+                    .nth(cnum.next_back().and_then(|c| c.to_digit(4)).unwrap() as usize)
+                    .unwrap()
+                    .to_string();
+                n = cnum
+                    .map(|c| c.to_digit(16).unwrap())
+                    .fold(0, |acc, n| acc * 16 + n)
+                    .to_string();
+            }
             (
-                Direction::from_char(dir.chars().next().unwrap()),
-                num.parse::<usize>().unwrap(),
-                // Why the color is here I really don't know
-                col.split(['(', '#', ')']).nth(2).unwrap(),
+                Direction::from_char(d.chars().next().unwrap()),
+                n.parse::<i64>().unwrap(),
             )
         })
         .collect();
 
-    let mut dug = HashSet::new();
+    let mut points = Vec::new();
     let mut pos = (0, 0);
-    dug.insert(pos);
-    for (dir, num, col) in instructions {
-        let offset = dir.offset();
-        for _ in 0..num {
-            pos = (pos.0 + offset.0, pos.1 + offset.1);
-            dug.insert(pos);
-        }
+    points.push(pos);
+    for (dir, num) in instructions {
+        let (ox, oy) = dir.offset();
+        pos = (pos.0 + ox * num, pos.1 + oy * num);
+        points.push(pos);
     }
 
-    let min_x = dug.iter().min_by_key(|p|p.0).unwrap().0;
-    let min_y = dug.iter().min_by_key(|p|p.1).unwrap().1;
-    let max_x = dug.iter().max_by_key(|p|p.0).unwrap().0;
-    let max_y = dug.iter().max_by_key(|p|p.1).unwrap().1;
-    dug = HashSet::from_iter(dug.iter().map(|(dx,dy)|(dx-min_x,dy-min_y)));
-    let mut ground = Vec::new();
-    let mut to_check = Vec::new();
-    for y in min_y..max_y+1 {
-        ground.push(Vec::new());
-        for x in min_y..max_x+1 {
-            // Map from -x..+y to 0..+x+y
-            let off_p = (x-min_x, y-min_y);
-            let is_dug = dug.contains(&off_p);
-            ground[off_p.1 as usize].push(is_dug);
-            if !is_dug {
-                to_check.push(off_p);
-            }
-            //print!("{}", if is_dug {'#'} else {'.'});
-        }
-        //println!();
+    // abs(shoelace sum) = total area * 2
+    let mut shoelace_sum = 0;
+    let mut border_points = 0;
+    for pair in points.windows(2) {
+        let [(x1, y1), (x2, y2)] = pair else {
+            panic!("How even")
+        };
+        shoelace_sum += (x2 + x1) * (y2 - y1);
+        border_points += i64::abs(y2 - y1) + i64::abs(x2 - x1);
     }
+    // Abs for protection against going counterclockwise
+    let area = i64::abs(shoelace_sum / 2);
 
-    let (bx,by) = (ground[0].len() as i32, ground.len() as i32);
-    //println!("{} by {} = {}", bx,by,bx*by);
-    
+    // Pick's Theorem
+    // A = I + B/2 - 1
+    // I = A - B/2 + 1
+    let interior = area - border_points / 2 + 1;
 
-    // Brute-force floodfill approach
-    // TODO: Use the better, formula-based way
-    while let Some(curr) = to_check.pop() {
-        let mut seen = HashSet::new();
-        let mut open = VecDeque::new();
-        let mut hit_outside = false;
-        open.push_back(curr);
-
-        // Flood fill type thing
-        while let Some(check) = open.pop_front() {
-            if seen.contains(&check) || dug.contains(&check) {
-                continue;
-            } else if check.0<0 || check.1<0 || check.0>bx || check.1>by {
-                hit_outside = true;
-                continue;
-            }
-            seen.insert(check);
-            for neighbour in Direction::all().iter().map(|d|{
-                let o = d.offset();
-                (check.0+o.0, check.1+o.1)
-            }) {
-                open.push_back(neighbour);
-            }
-        }
-
-        // As long as everything was contained, dig it out
-        if !hit_outside {
-            for seen_tile in &seen {
-                dug.insert(*seen_tile);
-            }
-        }
-
-        // If we've seen a tile, take it out of the list
-        to_check.retain(|t| !seen.contains(t));
-    }
-
-    // for y in 0..max_y+1 {
-    //     for x in 0..max_x+1 {
-    //         print!("{}", if dug.contains(&(x,y)) {'#'} else {'.'});
-    //     }
-    //     println!();
-    // }
-
-    println!("Num tiles dug: {}", dug.len());
+    println!("Total filled: {}", border_points + interior);
 }
